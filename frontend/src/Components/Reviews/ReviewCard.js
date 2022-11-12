fa9b00ff0039b5af376c3ca63a6513d8dd9246b2
Chris <hurdca1993@gmail.com>
2022-11-12 11:22:35 -0500

    Installed MUI to create star elements

--- frontend/src/Components/Reviews/ReviewCard.js
+++ frontend/src/Components/Reviews/ReviewCard.js
@@ -3,8 +3,10 @@ import { baseUrl } from '../../Shared/baseUrl';
 import axios from 'axios';
 import { setAuthHeader } from '../../Redux/token';
 import { useSelector } from 'react-redux';
+import Rating from '@mui/material/Rating';
 
-export default function ReviewCard() {
+
+export default function ReviewCard(props) {
 
     const [reviewedBeer,setReviewedBeer] = React.useState({})
     const [owner, setOwner] = React.useState({})  
@@ -19,16 +21,35 @@ export default function ReviewCard() {
     },[token])
 
     async function getReviewedBeer() {
-        
+        try {
+            // get beer of review 
+            let response = await axios.get(baseUrl + `/beers/${props.beerId}`);
+            // and save to state
+            setReviewedBeer(response.data);
+        } catch (ex) {
+            alert(ex);
+        }
     }
 
     async function getOwner() {
-
+        try {
+            // get owner of review 
+            let response = await axios.get(baseUrl + `/users/user/${props.userId}`);
+            // and save to state
+            setOwner(response.data);
+        } catch (ex) {
+            alert(ex);
+        }
     }
+
 
   return (
     <>
-
+        <img className='review--img' src={reviewedBeer.imgUrl} />
+        <p className='review--name'>Beer: {reviewedBeer.name}</p>
+        <p className='review--reviewer'>Reviewer: {owner.username}</p>
+        <Rating name="read-only" className='review--rating' value={props.rating} readOnly precision={0.5}/>
+        <textarea className='review--description' readOnly>{props.description}</textarea>
     </>
   )
 }
