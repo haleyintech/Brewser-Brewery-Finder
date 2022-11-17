d5253595e9288fb7b5186eaa515d2a483989654b
Chris <hurdca1993@gmail.com>
2022-11-17 08:41:43 -0500

    Implemented Create Review
    Create Review works on App, next step is to clean up visuals

--- frontend/src/Components/Reviews/ReviewInfo.js
+++ frontend/src/Components/Reviews/ReviewInfo.js
@@ -9,11 +9,13 @@ import { useSelector } from 'react-redux';
 import Rating from '@mui/material/Rating';
 import FilledBeerIcon from '../../assets/FilledBeerIcon';
 import EmptyBeerIcon from '../../assets/EmptyBeerIcon';
+import { func } from 'prop-types';
 
-function ReviewInfo(props) {
+function ReviewInfo() {
 
-    const[beer, setBeer] = React.useState({})
-    const[review, setReview] = React.useState({
+    const[beer, setBeer] = useState({})
+    const[brewery,setBrewery] = useState({})
+    const[review, setReview] =useState({
         userId: '',
         beerId: '',
         description: '',
@@ -25,12 +27,25 @@ function ReviewInfo(props) {
     const token = useSelector(state => state.token.token);
     const user = useSelector(state => state.user);
     const beerId = window.location.search.substring(8)
-    console.log(beerId)
 
     useEffect(() => {
         setAuthHeader(token);
         getBeer()
+        getUserId()
     }, [token]);
+
+    useEffect(()=>{
+        setReview(prev=>{
+            return {
+                ...prev,
+                beerId: beer.beerId
+            }
+        })
+        getBrewery()
+    },[beer])
+    
+
+    
 
 
     async function getBeer() {
@@ -38,21 +53,50 @@ function ReviewInfo(props) {
         setBeer(response.data)
     }
 
-    async function getUserId() {
-        const response = await axios.get(baseUrl + "/users/id/")
+    async function getBrewery() {
+        const response = await axios.get(baseUrl + `/breweries/${beer.breweryId}`)
+        setBrewery(response.data)
     }
 
-    function onRatingChange(event) {
+    async function getUserId() {
+        const response = await axios.get(baseUrl + `/users/id/?username=${user.username}`)
         setReview(prev=>{
             return {
                 ...prev,
-                rating: event.target.value
+                userId: response.data
+            }
+        }) 
+    }
+
+    function handleOnChange(event) {
+        const {name,value} = event.target
+        setReview(prev=>{
+            return {
+                ...prev,
+                [name]: value
             }
         })
     }
 
-    function saveReview() {
-        
+    function redirectToCaller(event) {
+        event.preventDefault();
+        window.history.back();
+    }
+
+
+    function saveReview(event) {
+        event.preventDefault()
+        axios.post(baseUrl + "/reviews",review)
+        .then(res=>{
+            if(res.status === 201) {
+                alert('Review Created')
+                redirectToCaller(event)
+            }
+        })
+        .catch(err=>{
+            alert('Failed to Create Review')
+            console.log(err.response)
+        })
     }
 
     console.log(review)
@@ -60,11 +104,19 @@ function ReviewInfo(props) {
     return (
         <div>
             <MainMenu/>
-            <form>
-                <label for='rating'>Rate this Beer: </label>
+            
+                <p className='beer-name'>Beer Name: {beer.name}</p>
+                <p className='brewery-name'>Brewery: {brewery.name}</p>
+            <form className='create-review-form' onSubmit={saveReview}>
+                <label htmlFor='rating'>Rate this Beer: </label>
                 <Rating name="rating" icon={<FilledBeerIcon />} emptyIcon={<EmptyBeerIcon />} 
-                defaultValue={0} precision={1} value={review.rating} onChange={onRatingChange} />
+                defaultValue={0} precision={1} value={review.rating} onChange={handleOnChange} />
+                <textarea value={review.description} onChange={handleOnChange} name='description' className='review-desc' placeholder='Describe Experience' />
+                <img width='30%' src={beer.imgUrl} />
+                <textarea className='' readOnly value={beer.description} />
+                <button>Create Review</button>                
             </form>
+                <button onClick={redirectToCaller}>Cancel</button>
         </div>
     )
 }
