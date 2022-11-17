325e038697aa2c59dc2fde2a42742311b1e4a566
Chris <hurdca1993@gmail.com>
2022-11-17 11:19:17 -0500

    Cleaned console log for phone format

--- frontend/src/Shared/phoneFormat.js
+++ frontend/src/Shared/phoneFormat.js
@@ -3,11 +3,5 @@ export default function phoneFormat(input) {
     if(typeof(input) !== 'string') input = input.toString()
     if(input.length === 10){
       return input.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
-    } else if(input.length < 10) {
-    console.log('was not supplied enough numbers please pass a 10 digit number')
-    } else if(input.length > 10) {
-      console.log('was supplied too many numbers please pass a 10 digit number')
-    }else{
-      console.log('something went wrong')
     }
   }
